eeeb9b3f244780441afef750f534aa4e16c2bcba
elliott.chenger <elliott.chenger@mutualmobile.com>
2015-03-13 23:36:43 -0500

    Adding unit test support

---
+++ mobile/src/test/java/TestTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by Elliott Chenger on 3/13/15.
+ */
+public class TestTest {
+
+    @Test
+    public void testThatTestsDoesntWork(){
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void testThatTestsWorks(){
+        assertEquals(1,1);
+    }
+}
